eeb13a17342840b296328523860b65fcb408e461
Lionel Sambuc <lionel.sambuc@epfl.ch>
2018-07-12 10:08:06 +0200

    Update License terms, fix typo

--- settings.sh
+++ settings.sh
@@ -1,4 +1,22 @@
-#Settings are taken in the following order of precedence:
+#                             Copyright (c) 2018-2018
+#            Data Intensive Applications and Systems Laboratory (DIAS)
+#                    Ecole Polytechnique Federale de Lausanne
+#
+#                               All Rights Reserved.
+#
+#       Permission to use, copy, modify and distribute this software and its
+#     documentation is hereby granted, provided that both the copyright notice
+#   and this permission notice appear in all copies of the software, derivative
+#   works or modified versions, and any portions thereof, and that both notices
+#                       appear in supporting documentation.
+#
+#   This code is distributed in the hope that it will be useful, but WITHOUT ANY
+#  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+#  A PARTICULAR PURPOSE. THE AUTHORS AND ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE
+# DISCLAIM ANY LIABILITY OF ANY KIND FOR ANY DAMAGES WHATSOEVER RESULTING FROM THE
+#                              USE OF THIS SOFTWARE.
+
+# Settings are taken in the following order of precedence:
 #  1. Shell Environment, or on the command line
 
 #  2. Node-specific settings `settings.local.<Alias>.sh`
